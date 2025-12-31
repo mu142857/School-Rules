9aa90b7744217d3541806f82978d1e37b37b6626
Mumu <linjiamu49@gmail.com>
2025-12-31 05:46:44 -0500

    Update info page

--- senior-year-of-high-school/ui/hud.gd
+++ senior-year-of-high-school/ui/hud.gd
@@ -4,16 +4,37 @@ extends CanvasLayer
 @onready var time_label = $Control/TimeLabel
 @onready var period_label = $Control/PeriodLabel
 @onready var buff_container = $Control/BuffContainer
-@onready var info_button = $Control/InfoButton
+@onready var info_button_pack = $Control/InfoButtonPack # pt，上官嘉木上官嘉木
+@onready var info_button = $Control/InfoButtonPack/InfoButton
 
+var hover_tween: Tween
 var pixel_font: Font
 
 func _ready():
-	pixel_font = load("res://shared/PixelFont.ttf")
-	time_label.add_theme_font_override("font", pixel_font)
-	period_label.add_theme_font_override("font", pixel_font)
-	
 	info_button.pressed.connect(_on_info_pressed)
+	info_button.mouse_entered.connect(_on_info_hover)
+	info_button.mouse_exited.connect(_on_info_unhover)
+
+func _on_info_hover():
+	if hover_tween:
+		hover_tween.kill()
+	hover_tween = create_tween()
+	hover_tween.tween_property(info_button_pack, "rotation_degrees", 3.0, 0.05)
+	hover_tween.tween_property(info_button_pack, "rotation_degrees", -3.0, 0.1)
+	hover_tween.parallel().tween_property(info_button_pack, "scale", Vector2(1.05, 1.05), 0.1)
+	hover_tween.tween_property(info_button_pack, "rotation_degrees", 0.0, 0.05)
+
+
+func _on_info_unhover():
+	if hover_tween:
+		hover_tween.kill()
+	hover_tween = create_tween()
+	hover_tween.tween_property(info_button_pack, "scale", Vector2(1.0, 1.0), 0.1)
+	info_button_pack.rotation_degrees = 0.0
+
+func _on_info_pressed():
+	pass
+	#InfoPanel.toggle()
 
 func _process(_delta):
 	update_time_display()
@@ -31,8 +52,5 @@ func update_time_display():
 	period_label.text = "%02d:%02d %s" % [GameManager.hour, GameManager.minute, period_text]
 
 func update_buff_display():
-	pass
-
-func _on_info_pressed():
-	# 之后连接InfoPanel
+	# 之后加buff图标
 	pass
